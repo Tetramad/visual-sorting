115d6e839df77df03f1db7a093bff22b85a184f3
Tetramad <cbwjd777@gmail.com>
2020-06-03 23:50:11 +0900

    feat: add prober read/write
    Signed-off-by: Tetramad <cbwjd777@gmail.com>

--- prober.cc
+++ prober.cc
@@ -44,7 +44,7 @@ auto Prober::swap(std::size_t lhs, std::size_t rhs) -> void {
   std::cout << "\033[1F\033[1G\033[0K" << std::ends;
 }
 
-auto Prober::less(std::size_t lhs, std::size_t rhs) -> bool {
+auto Prober::less(std::size_t lhs, std::size_t rhs) const -> bool {
   bool ret = vector[lhs] < vector[rhs];
   for (std::size_t i = 0; i < vector.size(); ++i) {
     std::cout << " ";
@@ -61,3 +61,33 @@ auto Prober::less(std::size_t lhs, std::size_t rhs) -> bool {
 }
 
 auto Prober::size(void) const -> std::size_t { return vector.size(); }
+
+auto Prober::read(std::size_t idx) const -> u16 {
+  for (std::size_t i = 0; i < vector.size(); ++i) {
+    std::cout << " ";
+    if (i == idx) {
+      std::cout << "\033[31m" << vector[i] << "\033[39m";
+    } else {
+      std::cout << vector[i];
+    }
+  }
+  std::cout << std::ends;
+  std::cin.get();
+  std::cout << "\033[1F\033[1G\033[0K" << std::ends;
+  return vector[idx];
+}
+
+auto Prober::write(std::size_t idx, u16 val) -> void {
+  vector[idx] = val;
+  for (std::size_t i = 0; i < vector.size(); ++i) {
+    std::cout << " ";
+    if (i == idx) {
+      std::cout << "\033[33m" << vector[i] << "\033[39m";
+    } else {
+      std::cout << vector[i];
+    }
+  }
+  std::cout << std::ends;
+  std::cin.get();
+  std::cout << "\033[1F\033[1G\033[0K" << std::ends;
+}
--- prober.hh
+++ prober.hh
@@ -4,7 +4,7 @@
 
 #include <cstdint>
 
-using u16 = short int;
+using u16 = unsigned short int;
 
 class Prober {
 public:
@@ -16,8 +16,10 @@ public:
   auto operator=(Prober &&other) noexcept -> Prober & = delete;
 
   auto swap(std::size_t lhs, std::size_t rhs) -> void;
-  auto less(std::size_t lhs, std::size_t rhs) -> bool;
+  auto less(std::size_t lhs, std::size_t rhs) const -> bool;
   auto size(void) const -> std::size_t;
+  auto read(std::size_t idx) const -> u16;
+  auto write(std::size_t idx, u16 val) -> void;
 
 private:
   std::vector<u16> vector;
